39d286a71b950362949b55131ceca79b886f4ff1
smonke <sonja.moenkemeyer@gmx.de>
2017-05-14 16:03:12 +0200

    building and pushing docs

--- publish.sh
+++ publish.sh
@@ -7,17 +7,16 @@ git add -A
 git commit -m "building and pushing docs"
 git push origin master
 # switch branches and pull the data we want
-sleep 10
 git checkout gh-pages -f
-sleep 20
 rm -rf .
 touch .nojekyll
+touch .gitignore
+echo '.idea' >> .gitignore
 git checkout master _build/html
 mv ./_build/html/* ./
 rm -rf ./_build/
 git add -A
 git commit -m "publishing updated docs..."
-sleep 10
 git push origin gh-pages
 # switch back
 git checkout master -f
